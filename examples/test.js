54f2463b2380170c24b59c1ee0c99ea8a04344d1
Alex Depatie <a.r.depatie@gmail.com>
2014-04-08 19:33:10 -0400

    Added test routes

--- examples/test.js
+++ examples/test.js
@@ -7,12 +7,33 @@ var request = require('request');
 var prompt = require('prompt');
 
 new vern().then(function($vern) {
+  $vern = require('vern-authentication')($vern);
   $vern = require('../lib')($vern);
   $vern.controllers.shopify = new $vern.controllers.ShopifyController($vern).init();
   request.get('http://0.0.0.0:3458/shopify/login_url', function(err, response) {
     console.log(response.body);
   });
 
+  var postData = {
+    product: {
+      title: 'Burton Custom Freestlye 151',
+      body_html: '<strong>Good snowboard!</strong>',
+      vendor: 'Burton',
+      product_type: 'Snowboard',
+      variants: [
+        {
+          option1: 'First',
+          price: '10.00',
+          sku: 123
+        },
+        {
+          option1: 'Second',
+          price: '20.00',
+          sku: '123'
+        }
+      ]
+    }
+  };
 
 
 
@@ -27,25 +48,30 @@ new vern().then(function($vern) {
 
       request.post('http://0.0.0.0:3458/shopify/authorization_code', {form: {code: result.code}}, function (err, response) {
         console.log(response.body);
-
         prompt.get(['access_token'], function (err, result) {
 
-          request.post('http://0.0.0.0:3458/shopify/register', {form: {access_token: result.access_token}}, function (err, response) {
+          request.post('http://0.0.0.0:3458/shopify/register', {form: {access_token: result.access_token, email: 'shopify@typefoo.com', password: 'test123'}}, function (err, response) {
             console.log(response.body);
-          });
 
+            request.post('http://0.0.0.0:3458/shopify/action', {form: { action: 'get', path: '/admin/orders.json', access_token: result.access_token}}, function (err, response) {
+              console.log(response.body);
+            });
+            request.post('http://0.0.0.0:3458/shopify/action', {form: { action: 'post', path: '/admin/products.json', data: postData, access_token: result.access_token}}, function (err, response) {
+              console.log(response.body);
+            });
+            request.post('http://0.0.0.0:3458/shopify/action', {form: { action: 'put', path: '/admin/products/12345.json', data: postData, access_token: result.access_token}}, function (err, response) {
+              console.log(response.body);
+            });
+            request.post('http://0.0.0.0:3458/shopify/action', {form: { action: 'delete', path: '/admin/products/12345.json', access_token: result.access_token}}, function (err, response) {
+              console.log(response.body);
+            });
+
+          });
         });
       });
     });
 
 
-
-
-
-
-  //request.post('http://0.0.0.0:3458/shopify/action', function(err, response) {
-    //console.log(response);
-  //});
 
 }).fail(function(err) {
   console.log(err);
